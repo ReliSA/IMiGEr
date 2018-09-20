33cc792c5bd30cde5a59f95027f3020bac4f261b
Tomáš Šimandl <tomas.simandl@gmail.com>
2018-09-20 14:03:31 +0200

    Cleaned sql create table script

--- sources/create_table.sql
+++ sources/create_table.sql
@@ -1,20 +1,7 @@
--- phpMyAdmin SQL Dump
--- version 4.4.14
--- http://www.phpmyadmin.net
---
--- Počítač: 127.0.0.1
--- Vytvořeno: Stř 11. kvě 2016, 16:08
--- Verze serveru: 5.6.26
--- Verze PHP: 5.6.12
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Databáze: `visualization_tool`
@@ -23,67 +10,34 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktura tabulky `diagram`
---
-
-CREATE TABLE IF NOT EXISTS `diagram` (
-  `id` int(11) NOT NULL,
-  `name` varchar(255) COLLATE utf8_czech_ci NOT NULL,
-  `created` datetime NOT NULL,
-  `last_update` datetime NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `public` tinyint(1) NOT NULL DEFAULT '0',
-  `graph_json` text COLLATE utf8_czech_ci NOT NULL DEFAULT ''
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
-
--- --------------------------------------------------------
-
---
 -- Struktura tabulky `user`
 --
 
 CREATE TABLE IF NOT EXISTS `user` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `nick` varchar(50) COLLATE utf8_czech_ci NOT NULL,
   `name` varchar(255) COLLATE utf8_czech_ci NOT NULL DEFAULT '',
   `psw` varchar(32) COLLATE utf8_czech_ci NOT NULL,
   `session` varchar(50) COLLATE utf8_czech_ci NOT NULL,
   `active` tinyint(1) NOT NULL,
   `created` datetime NOT NULL,
-  `email` varchar(50) COLLATE utf8_czech_ci NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
-
-
---
--- Klíče pro exportované tabulky
---
+  `email` varchar(50) COLLATE utf8_czech_ci NOT NULL,
+   CONSTRAINT PK_user PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
 
 --
--- Klíče pro tabulku `diagram`
---
-ALTER TABLE `diagram`
-  ADD PRIMARY KEY (`id`);
-
---
--- Klíče pro tabulku `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT pro tabulky
+-- Struktura tabulky `diagram`
 --
 
---
--- AUTO_INCREMENT pro tabulku `diagram`
---
-ALTER TABLE `diagram`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
---
--- AUTO_INCREMENT pro tabulku `user`
---
-ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+CREATE TABLE IF NOT EXISTS `diagram` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_czech_ci NOT NULL,
+  `created` datetime NOT NULL,
+  `last_update` datetime NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `public` tinyint(1) NOT NULL DEFAULT '0',
+  `graph_json` text COLLATE utf8_czech_ci NOT NULL DEFAULT '',
+  CONSTRAINT PK_diagram PRIMARY KEY (`id`),
+  CONSTRAINT  FK_user_diagram FOREIGN KEY (`user_id`) REFERENCES user(`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
+
