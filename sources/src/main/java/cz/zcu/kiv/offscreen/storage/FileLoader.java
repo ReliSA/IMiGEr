700a51d47236de68c23aadf2706288070bcb49f2
Tomáš Šimandl <tomas.simandl@gmail.com>
2018-10-08 08:27:19 +0200

    Added FileLoader class

---
+++ sources/src/main/java/cz/zcu/kiv/offscreen/storage/FileLoader.java
@@ -0,0 +1,66 @@
+package cz.zcu.kiv.offscreen.storage;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.log4j.Logger;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class FileLoader {
+
+    private static final int MAX_FILE_SIZE = 512000000;
+    private static final String ACCEPTED_EXTENSION_FILE = "json";
+
+    private Logger logger = Logger.getLogger(FileLoader.class);
+
+    /**
+     * Get multipart data from request which was send and return it as a map where key is field name from form and
+     * second value are data as string.
+     *
+     * @return loaded multipart data in map or empty map.
+     */
+    public Map<String, String> loadFile(HttpServletRequest request){
+
+        DiskFileItemFactory factory = new DiskFileItemFactory();
+
+        ServletFileUpload upload = new ServletFileUpload(factory);
+        upload.setSizeMax(MAX_FILE_SIZE);
+
+        Map<String, String> resultMap = new HashMap<>();
+
+        try {
+
+            List<FileItem> fileItems = upload.parseRequest(request);
+            for (FileItem item : fileItems) {
+                if (item.isFormField()) {
+                    resultMap.put(item.getFieldName(), item.getString());
+                } else {
+                    if (isAcceptedExtension(item.getName()) && item.getSize() > 0) {
+                        logger.debug(item.getName() + " - " + item.getContentType());
+                        resultMap.put(item.getFieldName(), item.getString("UTF-8"));
+                    } else {
+                        resultMap.put(item.getFieldName(), "");
+                    }
+                }
+            }
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return resultMap;
+    }
+
+    /**
+     * Check if input string ends with allowed extension.
+     *
+     * @param componentName all name of file
+     * @return true - if extension is allowed, false - otherwise.
+     */
+    private boolean isAcceptedExtension(String componentName) {
+        return componentName.endsWith("." + ACCEPTED_EXTENSION_FILE);
+    }
+}
