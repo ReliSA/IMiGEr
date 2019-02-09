348cfce7133bdecbe89000e4ef79458facac5b44
Tomáš Šimandl <tomas.simandl@gmail.com>
2019-02-09 14:40:11 +0100

    #23 extend DB column for saving diagram JSON
    - updated DB graph_json datatype

--- sources/imiger-core/create_table.sql
+++ sources/imiger-core/create_table.sql
@@ -36,7 +36,7 @@ CREATE TABLE IF NOT EXISTS `diagram` (
   `last_update` datetime NOT NULL,
   `user_id` int(11) NOT NULL,
   `public` tinyint(1) NOT NULL DEFAULT '0',
-  `graph_json` text COLLATE utf8_czech_ci NOT NULL DEFAULT '',
+  `graph_json` longtext COLLATE utf8_czech_ci NOT NULL DEFAULT '',
   CONSTRAINT PK_diagram PRIMARY KEY (`id`),
   CONSTRAINT  FK_user_diagram FOREIGN KEY (`user_id`) REFERENCES user(`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
