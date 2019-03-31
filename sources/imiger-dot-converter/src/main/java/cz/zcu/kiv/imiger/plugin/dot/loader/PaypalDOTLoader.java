8ade384b634ab37088454414b246865dcfebdf6c
Vít Mazín <vit.mazin@seznam.cz>
2019-03-31 17:25:12 +0200

    Loader fix

--- sources/imiger-dot-converter/src/main/java/cz/zcu/kiv/imiger/plugin/dot/loader/PaypalDOTLoader.java
+++ sources/imiger-dot-converter/src/main/java/cz/zcu/kiv/imiger/plugin/dot/loader/PaypalDOTLoader.java
@@ -21,8 +21,14 @@ public class PaypalDOTLoader extends BaseDOTLoader<VertexDTO, EdgeDTO> {
     private final List<VertexDTO> vertices;
     private final List<EdgeDTO> edges;
     private final HashSet<AttributeType> attributeTypes;
+    private final HashSet<String> attributeNames;
     private final HashMap<GraphNode, Integer> remappedVertices;
 
+    /**
+     * DOT loader which uses Digraph library
+     *
+     * @param dotInput - Source DOT file
+     */
     public PaypalDOTLoader(String dotInput) {
         super(dotInput);
         parser = null;
@@ -36,33 +42,58 @@ public class PaypalDOTLoader extends BaseDOTLoader<VertexDTO, EdgeDTO> {
         edges = new ArrayList<>();
         attributeTypes = new HashSet<>();
         remappedVertices = new HashMap<>();
-        processEdges();
+        attributeNames = new HashSet<>();
         processVertices();
+        processEdges();
     }
 
+    /**
+     * Returns list of vertices DTO
+     * @return List of vertices DTO
+     */
     @Override
     public List<VertexDTO> getVertices() {
         return vertices;
     }
 
+    /**
+     * Returns list of edges DTO
+     * @return List of edges DTO
+     */
     @Override
     public List<EdgeDTO> getEdges() {
         return edges;
     }
 
+    /**
+     * Returns list of attribute types
+     * @return List of attribute types
+     */
     @Override
     public Set<AttributeType> getAttributeTypes() {
+        if(attributeTypes.size() == 0) {
+            for(String attr : attributeNames) {
+                attributeTypes.add(new AttributeType(attr, AttributeDataType.STRING, ""));
+            }
+        }
         return attributeTypes;
     }
 
+    /**
+     * Method transforms vertices from GraphNode object to vertex DTO and stores attribute type.
+     * It has to be called before processing edges.
+     */
     private void processVertices() {
         int id = 0;
 
         for(GraphNode node : this.parser.getNodes().values()) {
             HashMap<String, String> attrs = new HashMap<>();
-            for(Map.Entry<String, Object> entry : node.getAttributes().entrySet()) {
-                attrs.put(entry.getKey(), entry.getValue().toString());
-                attributeTypes.add(new AttributeType(entry.getKey(), AttributeDataType.STRING, ""));
+
+            if(!node.getAttributes().entrySet().isEmpty()) {
+                for (Map.Entry<String, Object> entry : node.getAttributes().entrySet()) {
+                    attrs.put(entry.getKey(), entry.getValue().toString());
+                    attributeNames.add(entry.getKey());
+                }
             }
 
             VertexDTO vertex = new VertexDTO(node.getId(), id++, attrs);
@@ -72,14 +103,21 @@ public class PaypalDOTLoader extends BaseDOTLoader<VertexDTO, EdgeDTO> {
         }
     }
 
+    /**
+     * Method transforms vertices from GraphNode object to vertex DTO and stores attribute type.
+     * It has to be called after processing vertices.
+     */
     private void processEdges() {
         int id = 0;
 
         for(GraphEdge edge : this.parser.getEdges().values()) {
             HashMap<String, String> attrs = new HashMap<>();
-            for(Map.Entry<String, Object> entry : edge.getAttributes().entrySet()) {
-                attrs.put(entry.getKey(), entry.getValue().toString());
-                attributeTypes.add(new AttributeType(entry.getKey(), AttributeDataType.STRING, ""));
+
+            if(!edge.getAttributes().entrySet().isEmpty()) {
+                for (Map.Entry<String, Object> entry : edge.getAttributes().entrySet()) {
+                    attrs.put(entry.getKey(), entry.getValue().toString());
+                    attributeNames.add(entry.getKey());
+                }
             }
 
             int from = remappedVertices.get(edge.getNode1());
