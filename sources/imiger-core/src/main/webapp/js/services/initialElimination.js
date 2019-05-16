6d678302aa358c74615d66e3249eb70e6dc7e442
Patrik Harag <patrikharag@gmail.com>
2019-05-16 20:34:45 +0200

    Group by archetype if possible (refs #7468)

--- sources/imiger-core/src/main/webapp/js/services/initialElimination.js
+++ sources/imiger-core/src/main/webapp/js/services/initialElimination.js
@@ -21,15 +21,54 @@ class InitialElimination {
                     visibleComponentsCount - this._maxVisibleComponents);
 
             if (numberOfVerticesToGroup > 0) {
-                // pack the first n vertices into a group
-                let group = Group.create();
-                visibleVertices.slice(0, numberOfVerticesToGroup).forEach(vertex => {
-                    group.addVertex(vertex);
-                });
-                app.nodeList.push(group);
-                app.groupList.push(group);
-                app.viewportComponent.addNode(group);
+                if (!this._groupByTypes(visibleVertices)) {
+                    this._simpleGrouping(visibleVertices, numberOfVerticesToGroup);
+                }
             }
         }
+    }
+
+    _groupByTypes(visibleVertices) {
+        let groups = new Map();
+        visibleVertices.forEach(vertex => {
+            let entry = groups.get(vertex.archetype);
+            if (entry !== undefined) {
+                entry.push(vertex);
+            } else {
+                groups.set(vertex.archetype, [vertex]);
+            }
+        });
+
+        if (groups.size > 2) {
+            // group by types
+            groups.forEach((v, k) => {
+                let archetype = app.archetype.vertex[k];
+                let groupName = archetype.name;
+                this._createGroup(v, groupName);
+            });
+            return true;
+        } else {
+            // not enough vertex types (archetypes)
+            return false;
+        }
+    }
+
+    _simpleGrouping(visibleVertices, numberOfVerticesToGroup) {
+        // just pack the first n vertices into a group
+        this._createGroup(visibleVertices.slice(0, numberOfVerticesToGroup), null);
+    }
+
+    _createGroup(vertices, name) {
+        let group = Group.create();
+        vertices.forEach(vertex => {
+            group.addVertex(vertex);
+        });
+        if (name !== null) {
+            group.name = name;
+        }
+
+        app.nodeList.push(group);
+        app.groupList.push(group);
+        app.viewportComponent.addNode(group);
     }
 }
