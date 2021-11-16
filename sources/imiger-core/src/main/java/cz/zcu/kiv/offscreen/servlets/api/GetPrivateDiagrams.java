54da3e9642b2e1a13cd68ebe4c3ab3939d897592
Jan Pašek <pasekj@students.zcu.cz>
2021-11-16 15:14:11 +0100

    #2 Added docstrings to the unused GetPrivateDiagrams, that will replace the UploadGraph JSP page

--- sources/imiger-core/src/main/java/cz/zcu/kiv/offscreen/servlets/api/GetPrivateDiagrams.java
+++ sources/imiger-core/src/main/java/cz/zcu/kiv/offscreen/servlets/api/GetPrivateDiagrams.java
@@ -13,6 +13,10 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * This servlet is used to query all private diagrams of the currently
+ * logged-in user via a RestAPI
+ */
 public class GetPrivateDiagrams extends BaseServlet {
 
     private static final Logger logger = LogManager.getLogger();
@@ -21,32 +25,36 @@ public class GetPrivateDiagrams extends BaseServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         logger.debug("Processing request");
 
+        // Verify whether user is logged in
         if (!isLoggedIn(request)) {
             response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
             logger.debug("User is unauthorized");
             return;
         }
 
+        // Get all user's private diagrams from the DAO
         DiagramDAO diagramDAO = new DiagramDAO();
-
         int loggedUserId = getUserId(request);
         List<Map<String, Object>> userDiagramList = diagramDAO.getDiagramsByUserId(loggedUserId);
 
-        JsonArray jsonArray = new JsonArray();
+        // Parse the list of the diagrams into a JSON
+        JsonArray privateDiagramsJSON = new JsonArray();
         for (Map<String, Object> userDiagram : userDiagramList) {
 
-            JsonObject jsonObject = new JsonObject();
-            jsonObject.addProperty("id", String.valueOf(userDiagram.get("id")));
-            jsonObject.addProperty("name", String.valueOf(userDiagram.get("name")));
-            jsonObject.addProperty("created", String.valueOf(userDiagram.get("created")));
-            jsonObject.addProperty("last_update", String.valueOf(userDiagram.get("last_update")));
-            jsonArray.add(jsonObject);
+            // Create a JSON object that represents one diagram and append it into the array of diagrams
+            JsonObject diagramJSON = new JsonObject();
+            diagramJSON.addProperty("id", String.valueOf(userDiagram.get("id")));
+            diagramJSON.addProperty("name", String.valueOf(userDiagram.get("name")));
+            diagramJSON.addProperty("created", String.valueOf(userDiagram.get("created")));
+            diagramJSON.addProperty("last_update", String.valueOf(userDiagram.get("last_update")));
+            privateDiagramsJSON.add(diagramJSON);
         }
 
+        // Return the response
         response.setContentType("application/json");
         response.setCharacterEncoding("UTF-8");
         response.setStatus(HttpServletResponse.SC_OK);
-        response.getWriter().write(jsonArray.toString());
+        response.getWriter().write(privateDiagramsJSON.toString());
         response.getWriter().flush();
         logger.debug("Response OK");
 
