d9de660eb5726a91b9996792fa53bd85bfb3ff97
Stanislav Král <stanislav.kral3@gmail.com>
2021-12-05 14:01:11 +0100

    Added ExcludedVertex component.

---
+++ sources/frontend/src/components/ExcludedVertex.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="py-3 px-2 mb-3 rounded border border-secondary">
+    {{title}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ExcludedVertex",
+  props: {
+    title: String
+  }
+}
+</script>
