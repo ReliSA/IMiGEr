01f20bf3f19c559e192c4aacece1c174b7c78b7e
Stanislav Král <stanislav.kral3@gmail.com>
2021-12-07 12:38:58 +0100

    Fixed a bug that resulted in text being selected when performing canvas translation.

--- sources/frontend/src/components/svg/SvgCanvas.vue
+++ sources/frontend/src/components/svg/SvgCanvas.vue
@@ -154,6 +154,7 @@ export default {
           this.changeTranslation({dx: dx, dy: dy})
         }
       }
+      event.preventDefault();
     },
     onVertexPositionChanged(payload) {
       this.changeVertexPos(payload)
