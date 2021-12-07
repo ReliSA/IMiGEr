f4e938534ebd202338bf19a94e776b498940cf1e
janpasek97 <janpasek97@gmail.com>
2021-12-07 12:59:59 +0100

    Issue #8 Positioning minimap

--- sources/frontend/src/components/SvgShowcase.vue
+++ sources/frontend/src/components/SvgShowcase.vue
@@ -1,27 +1,26 @@
 <!-- A component composing together a SVG canvas and its minimap -->
 <template>
-  <div class="container-fluid d-flex flex-column h-100">
+  <div class="row flex-grow-1 d-flex">
     <div class="py-3">
       <p class="display-6 font-weight-bold"><strong>IMiGEr</strong></p>
       <ClickBehaviourToggle :behaviour="this.$store.state.clickBehaviour"
                             :behaviour-changed="this.clickBehaviourChanged"/>
     </div>
-    <div class="row flex-grow-1">
-      <div class="col-md-9 no-padding d-flex flex-column h-100">
-        <svg-canvas
-            id="main"
-            :view-port="this.$store.state.viewPort"
-            :vertices="this.$store.state.vertices"
-            :edges="this.$store.state.edges"
-            :excluded-vertices-boxes="this.$store.state.excludedNodesClientRects"
-            :vertex_map="this.$store.state.vertex_map"
-            :style="this.$store.state.style"/>
-      </div>
-      <div class="col-md-3 no-padding flex-grow-1">
+    <div class="p-0">
+      <svg-canvas
+          id="main"
+          :view-port="this.$store.state.viewPort"
+          :vertices="this.$store.state.vertices"
+          :edges="this.$store.state.edges"
+          :excluded-vertices-boxes="this.$store.state.excludedNodesClientRects"
+          :vertex_map="this.$store.state.vertex_map"
+          :style="this.$store.state.style"/>
+    </div>
+      <div class="p-0" style="opacity: 0.9">
         <svg-minimap
             class="canvas-minimap"
             ref-id="main"
-            :view-port="{height: 300}"
+            :view-port="{height: 300, width: 300}"
             :parent-world-size="this.$store.state.worldSize"
             :parent-view-port="this.$store.state.viewPort"
         />
@@ -35,7 +34,6 @@
                           :on-client-box-removed="() => removeExcludedVertexClientRect(vertex)"
           />
         </div>
-      </div>
     </div>
   </div>
 </template>
